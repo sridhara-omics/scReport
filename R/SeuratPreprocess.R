dac6866bbf22ed29f0c9a2a1f6fdd073189d0476
Viswanadham Sridhara <sridhara.omics@gmail.com>
2024-05-21 20:24:55 +0530

    edit preprocess R script

--- R/SeuratPreprocess.R
+++ R/SeuratPreprocess.R
@@ -1,6 +1,12 @@
-library(Seurat)
-
-SeuratPreprocess <- function(counts_data) {
+#' Create a Seurat object from count data
+#'
+#' This function creates a Seurat object from count data using Seurat::CreateSeuratObject.
+#'
+#' @export
+#' @importFrom Seurat CreateSeuratObject
+#' @param counts_data A matrix or data frame of count data.
+#' @param ... Additional arguments to be passed to Seurat::CreateSeuratObject.
+#' @return A Seurat object.
+SeuratPreprocess <- function(counts_data, ...) {
   seurat_object <- Seurat::CreateSeuratObject(counts = counts_data, project = "project_title", min.cells = 3, min.features = 200)
-  return(seurat_object)
 }
